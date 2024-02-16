0d0feebd605aab10f191665be26038aa9de377b1
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2024-02-16 15:09:53 -0600

    Add files via upload

--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -3,13 +3,17 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   const token = req.headers['authorization'];
   if (!token) {
-    return res.status(403).send('Se requiere un token para la autenticación');
+    // Redireccionar al cliente a la página de inicio.
+    // Esto solo funcionará correctamente para solicitudes GET desde un navegador.
+    return res.redirect('/#inicio');
   }
   
   try {
     const decoded = jwt.verify(token, 'tu_secreto');
     req.user = decoded;
   } catch (error) {
+    // Enviar un estado de error y un mensaje indicando que el token no es válido.
+    // Para las APIs, es mejor enviar un estado HTTP en lugar de redirigir.
     return res.status(401).send('Token no válido');
   }
 
